11c07c90e9381a70b6d4f5448e226b76af74143d
Eric White <eric.white.austin@gmail.com>
2024-02-26 12:02:19 -0600

    Add discord link and beta icon to TopBar.

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {AppBar, Toolbar} from "@mui/material";
-import GitHubIcon from "@mui/icons-material/GitHub";
 import DeleteIcon from '@mui/icons-material/Delete';
+import {Icon} from '@iconify/react';
 import logo from '../assets/Logo.png';
 
 interface TopBarProps {
@@ -27,20 +27,37 @@ const TopBar: React.FC<TopBarProps> = ({onDeleteData, showDeleteButton}) => {
                         }}
                     />
                     <p style={{margin: '0', color: 'white', fontSize: '25px'}}>Runelogs</p>
+                    <Icon
+                        icon="clarity:beta-solid"
+                        style={{
+                            marginLeft: '10px'
+                        }}
+                    />
                 </div>
-                <div style={{display: 'flex', alignItems: 'center'}}>
+                <div style={{display: 'flex', alignItems: 'center',}}>
                     {showDeleteButton && (
                         <DeleteIcon
                             onClick={onDeleteData}
                             style={{fontSize: 35, cursor: 'pointer', color: 'white', marginRight: '30px'}}
                         />
                     )}
-                    <a href="https://github.com/SuperNerdEric/runelogs" target="_blank" style={{textDecoration: "none"}}
-                       rel="noreferrer">
-                        <GitHubIcon style={{
-                            fontSize: 35,
-                            color: "white"
-                        }}/>
+                    <a href="https://discord.gg/ZydwX7AJEd" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit', display: 'flex', alignItems: 'center', justifyContent: 'center', marginRight: '15px' }}>
+                        <Icon
+                            icon="logos:discord-icon"
+                            style={{
+                                width: '35px',
+                                height: '35px',
+                            }}
+                        />
+                    </a>
+                    <a href="https://github.com/SuperNerdEric/runelogs" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <Icon
+                            icon="bi:github"
+                            style={{
+                                width: '35px',
+                                height: '33px',
+                            }}
+                        />
                     </a>
                 </div>
             </Toolbar>
