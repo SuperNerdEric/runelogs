f9a9bf4258a128d3869e950a0c0b71303e980100
Eric White <eric.white.austin@gmail.com>
2026-06-09 12:50:14 -0500

    Update generateEquipment.py

--- scripts/generateEquipment.py
+++ scripts/generateEquipment.py
@@ -1,10 +1,9 @@
 """
-    Script to generate an equipment.json of all the equipment on the OSRS Wiki, and downloads images for each item.
+    Script to generate an equipment.json of all the equipment on the OSRS Wiki.
     The JSON file is placed in ../src/lib/equipment.json.
 
     Written for Python 3.9.
 """
-import os
 import requests
 import json
 import urllib.parse
@@ -13,96 +12,92 @@ FILE_NAME = 'src/lib/equipment.json'
 WIKI_BASE = 'https://oldschool.runescape.wiki'
 API_BASE = WIKI_BASE + '/api.php'
 
-REQUIRED_PRINTOUTS = [
-    'Crush attack bonus',
-    'Crush defence bonus',
-    'Equipment slot',
-    'Item ID',
-    'Magic Damage bonus',
-    'Magic attack bonus',
-    'Magic defence bonus',
-    'Prayer bonus',
-    'Range attack bonus',
-    'Ranged Strength bonus',
-    'Range defence bonus',
-    'Slash attack bonus',
-    'Slash defence bonus',
-    'Stab attack bonus',
-    'Stab defence bonus',
-    'Strength bonus',
-    'Version anchor',
-    'Weapon attack range',
-    'Weapon attack speed',
-    'Combat style'
+BUCKET_API_FIELDS = [
+    'page_name',
+    'page_name_sub',
+    'item_id',
+    'version_anchor',
+    'infobox_bonuses.equipment_slot',
+    'infobox_bonuses.weapon_attack_speed',
+    'infobox_bonuses.combat_style',
 ]
 
 def getEquipmentData():
-    equipment = {}
+    equipment = []
     offset = 0
+    fields_csv = ",".join(map(repr, BUCKET_API_FIELDS))
     while True:
         print('Fetching equipment info: ' + str(offset))
         query = {
-            'action': 'ask',
+            'action': 'bucket',
             'format': 'json',
-            'query': '[[Equipment slot::+]][[Item ID::+]]|?' + '|?'.join(REQUIRED_PRINTOUTS) + '|limit=500|offset=' + str(offset)
+            'query':
+            (
+                f"bucket('infobox_item')"
+                f".select({fields_csv})"
+                f".limit(500).offset({offset})"
+                f".where('infobox_bonuses.equipment_slot', '!=', bucket.Null())"
+                f".where('item_id', '!=', bucket.Null())"
+                f".join('infobox_bonuses', 'infobox_bonuses.page_name_sub', 'infobox_item.page_name_sub')"
+                f".orderBy('page_name_sub', 'asc').run()"
+            )
         }
+
         r = requests.get(API_BASE + '?' + urllib.parse.urlencode(query), headers={
             'User-Agent': 'osrs-dps-calc (https://github.com/weirdgloop/osrs-dps-calc)'
         })
+
         data = r.json()
 
-        if 'query' not in data or 'results' not in data['query']:
-            # No results?
+        if 'bucket' not in data:
             break
 
-        equipment = equipment | data['query']['results']
+        equipment = equipment + data['bucket']
 
-        if 'query-continue-offset' not in data or int(data['query-continue-offset']) < offset:
-            # If we are at the end of the results, break out of this loop
-            break
+        if len(data['bucket']) == 500:
+            offset += 500
         else:
-            offset = data['query-continue-offset']
+            break
+
     return equipment
 
 
-def getPrintoutValue(prop):
-    # SMW printouts are all arrays, so ensure that the array is not empty
-    if not prop:
-        return None
-    else:
-        return prop[0]
-
-
 def main():
-    # Grab the equipment info using SMW, including all the relevant printouts
     wiki_data = getEquipmentData()
 
-    # Convert the data into our own JSON structure
     data = []
+    seen = set()
 
-    # Loop over the equipment data from the wiki
-    for k, v in wiki_data.items():
-        print('Processing ' + k)
-        # Sanity check: make sure that this equipment has printouts from SMW
-        if 'printouts' not in v:
-            print(k + ' is missing SMW printouts - skipping.')
+    for v in wiki_data:
+        page_name_sub = v['page_name_sub']
+        if page_name_sub in seen:
+            continue
+        seen.add(page_name_sub)
+
+        print('Processing ' + page_name_sub)
+
+        try:
+            item_id = int(v.get('item_id')[0]) if v.get('item_id') else None
+        except ValueError:
+            print("Skipping - invalid item ID (not an int)")
             continue
 
-        po = v['printouts']
-        item_id = getPrintoutValue(po['Item ID'])
+        if item_id is None:
+            continue
+
         equipment = {
-            'name': k.rsplit('#', 1)[0],
+            'name': v['page_name'],
             'id': item_id,
-            'version': getPrintoutValue(po['Version anchor']) or '',
-            'slot': getPrintoutValue(po['Equipment slot']) or '',
-            'category': getPrintoutValue(po['Combat style']) or '',
+            'version': v.get('version_anchor', '') or '',
+            'slot': v.get('infobox_bonuses.equipment_slot', '') or '',
+            'category': v.get('infobox_bonuses.combat_style', '') or '',
             'isTwoHanded': False
         }
 
-        combat_style = getPrintoutValue(po['Combat style']) or ''
-        weapon_speed = int(getPrintoutValue(po['Weapon attack speed']) or 0)
+        combat_style = v.get('infobox_bonuses.combat_style', '') or ''
+        weapon_speed = int(v.get('infobox_bonuses.weapon_attack_speed', 0) or 0)
 
-        # Subtract weapon speed for ranged weapon assuming using rapid style
+        # Subtract weapon speed for ranged weapons assuming using rapid style
         if combat_style in ['Bow', 'Chinchompas', 'Crossbow', 'Salamander', 'Thrown']:
             speed = max(weapon_speed - 1, 0)
         else:
@@ -119,9 +114,7 @@ def main():
         if equipment['version'] == 'Nightmare Zone':
             equipment['version'] = ''
 
-        # Append the current equipment item to the calc's equipment list
         data.append(equipment)
-
 
     print('Total equipment: ' + str(len(data)))
     data.sort(key=lambda d: d.get('name'))
