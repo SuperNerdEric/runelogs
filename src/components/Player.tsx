9bd53773834eae72b284d19029cb4fdda91c42c5
Eric White <eric.white.austin@gmail.com>
2026-06-13 15:08:36 -0500

    Add player avatar.

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,14 +5,27 @@ import PersonalBests from './PersonalBests';
 import RecentEncounters from "./RecentEncounters";
 import { contentColumnSx, colors } from '../theme';
 import { displayUsername } from '../utils/utils';
+import playerAvatar from '../assets/player-avatar.png';
 
 const Player: React.FC = () => {
     const { playerName } = useParams<{ playerName: string }>();
 
     return (
         <Box sx={{...contentColumnSx, mt: 1, px: 2, pb: 0, textAlign: 'left'}}>
-            <Box pb={0} pt={0}>
-                <Typography variant="h4" gutterBottom sx={{textTransform: 'capitalize', color: colors.text.player}}>
+            <Box pb={0} pt={0} display="flex" alignItems="center" gap={1.5}>
+                <Box
+                    component="img"
+                    src={playerAvatar}
+                    alt={playerName ? `${displayUsername(playerName)} avatar` : 'Player avatar'}
+                    sx={{
+                        width: 40,
+                        height: 40,
+                        flexShrink: 0,
+                        objectFit: 'cover',
+                        borderRadius: '4px',
+                    }}
+                />
+                <Typography variant="h4" gutterBottom sx={{textTransform: 'capitalize', color: colors.text.player, m: 0}}>
                     {displayUsername(playerName)}
                 </Typography>
             </Box>
