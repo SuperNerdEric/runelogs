ec310c7202730ce01dbfffe11b27ac3858f70fa2
Eric White <eric.white.austin@gmail.com>
2026-06-14 21:32:03 -0500

    Rework help page.

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -1,40 +1,138 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {
-    Box,
-    Typography,
-    Link,
     Accordion,
-    AccordionSummary,
     AccordionDetails,
+    AccordionSummary,
+    Box,
+    Link,
+    Typography,
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { useLocation } from 'react-router-dom';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import {useLocation} from 'react-router-dom';
 import SectionBox from './SectionBox';
 import FindCombatLogIcon from '../assets/help/find_combat_log.png';
 import BrightHitsplat from '../assets/help/bright_hitsplat.png';
 import TintedHitsplat from '../assets/help/tinted_hitsplat.png';
 import PanelIcon from '../assets/help/panel_icon.png';
 import FolderIcon from '../assets/help/folder_icon.png';
+import {colors, contentColumnSx, media} from '../theme';
 
 type FaqItem = { id: string; title: string; body: React.ReactNode };
 
+const helpIconInlineSx = {
+    height: '1em',
+    width: 'auto',
+    display: 'inline-block',
+    verticalAlign: '-0.125em',
+    mx: 0.25,
+};
+
+const bodyTextSx = {
+    color: colors.text.primary,
+    lineHeight: 1.6,
+    mb: 1.5,
+    '&:last-child': {mb: 0},
+};
+
+const accordionSx = {
+    bgcolor: colors.background.surfaceAlt,
+    border: `1px solid ${colors.border.default}`,
+    borderRadius: '5px !important',
+    boxShadow: 'none',
+    mb: 1.5,
+    '&:before': {display: 'none'},
+    '&.Mui-expanded': {mb: 1.5},
+};
+
+const accordionSummarySx = {
+    px: 2,
+    '& .MuiAccordionSummary-content': {
+        my: 1.25,
+    },
+    '& .MuiAccordionSummary-expandIconWrapper': {
+        color: colors.text.primary,
+    },
+};
+
+const accordionDetailsSx = {
+    px: 2,
+    pb: 2.5,
+    pt: 1.5,
+    borderTop: `1px solid ${colors.border.default}`,
+    '& .MuiTypography-root': {
+        ...bodyTextSx,
+        '& a, & .MuiLink-root': {
+            color: colors.text.link,
+            '&:hover': {
+                color: colors.text.link,
+            },
+        },
+    },
+};
+
 const Help: React.FC = () => {
-    const { hash } = useLocation();
-    const openId = React.useMemo(() => (hash ? hash.slice(1) : null), [hash]);
+    const {hash} = useLocation();
+    const openId = useMemo(() => (hash ? hash.slice(1) : null), [hash]);
+    const [expandedId, setExpandedId] = useState<string | false>(openId ?? false);
+
+    useEffect(() => {
+        if (!openId) {
+            return;
+        }
+
+        setExpandedId(openId);
+        window.requestAnimationFrame(() => {
+            document.getElementById(openId)?.scrollIntoView({behavior: 'smooth', block: 'start'});
+        });
+    }, [openId]);
 
     return (
-        <Box display="flex" flexDirection="column" alignItems="center" mt={4} px={2}>
-            <SectionBox sx={{ p: 4 }}>
-                <Typography variant="h3" gutterBottom sx={{ color: 'white' }}>
+        <Box sx={{...contentColumnSx, mt: 2, px: 2, pb: 4, [media.mobileDown]: {px: 1}}}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: 1.5,
+                    mb: 3,
+                    pt: 1,
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        width: 56,
+                        height: 56,
+                        borderRadius: 2,
+                        bgcolor: colors.background.surfaceAlt,
+                        border: `1px solid ${colors.border.default}`,
+                    }}
+                >
+                    <HelpOutlineIcon sx={{fontSize: 32, color: colors.upload.dragActive}}/>
+                </Box>
+                <Typography variant="h4" sx={{m: 0, fontWeight: 600, color: colors.text.primary}}>
                     Frequently Asked Questions
                 </Typography>
+            </Box>
 
-                {faq.map(({ id, title, body }) => (
-                    <Accordion key={id} id={id} defaultExpanded={id === openId}>
-                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                            <Typography variant="h6">{title}</Typography>
+            <SectionBox sx={{p: {xs: 2.5, md: 4}}}>
+                {faq.map(({id, title, body}) => (
+                    <Accordion
+                        key={id}
+                        id={id}
+                        expanded={expandedId === id}
+                        onChange={(_, isExpanded) => setExpandedId(isExpanded ? id : false)}
+                        disableGutters
+                        sx={accordionSx}
+                    >
+                        <AccordionSummary expandIcon={<ExpandMoreIcon/>} sx={accordionSummarySx}>
+                            <Typography variant="h6" sx={{fontWeight: 600, color: colors.text.primary}}>
+                                {title}
+                            </Typography>
                         </AccordionSummary>
-                        <AccordionDetails>{body}</AccordionDetails>
+                        <AccordionDetails sx={accordionDetailsSx}>{body}</AccordionDetails>
                     </Accordion>
                 ))}
             </SectionBox>
@@ -49,9 +147,9 @@ const faq: FaqItem[] = [
         id: 'what-is-runelogs',
         title: 'What is Runelogs?',
         body: (
-            <Typography paragraph>
+            <Typography variant="body1">
                 Runelogs is a combat log analysis tool for Old School RuneScape that works with the{' '}
-                <Link href="https://runelite.net/plugin-hub/show/combat-logger" target="_blank" rel="noopener">
+                <Link href="https://runelite.net/plugin-hub/show/combat-logger" target="_blank" rel="noopener noreferrer">
                     Combat Logger
                 </Link>{' '}
                 plugin to help players review fights, track performance, and improve strategy.
@@ -64,14 +162,23 @@ const faq: FaqItem[] = [
         title: 'Where do I find my combat log files?',
         body: (
             <>
-                <Typography paragraph>
-                    1. Click the Combat Logger <img src={PanelIcon} alt="Panel Icon" height={16}/> panel icon in the RuneLite sidebar.
+                <Typography variant="body1">
+                    1. Click the Combat Logger{' '}
+                    <Box component="img" src={PanelIcon} alt="Panel Icon" sx={helpIconInlineSx}/>
+                    {' '}panel icon in the RuneLite sidebar.
                 </Typography>
-                <Typography paragraph>
-                    2. Click the Folder <img src={FolderIcon} alt="folder Icon" height={16}/> icon to open your combat log folder.
+                <Typography variant="body1">
+                    2. Click the Folder{' '}
+                    <Box component="img" src={FolderIcon} alt="Folder Icon" sx={helpIconInlineSx}/>
+                    {' '}icon to open your combat log folder.
                 </Typography>
                 <Box display="flex" justifyContent="center" mt={2}>
-                    <img src={FindCombatLogIcon} alt="Find Combat Log" width={300} />
+                    <Box
+                        component="img"
+                        src={FindCombatLogIcon}
+                        alt="Find Combat Log"
+                        sx={{maxWidth: 300, width: '100%', height: 'auto'}}
+                    />
                 </Box>
             </>
         ),
@@ -80,10 +187,11 @@ const faq: FaqItem[] = [
         id: 'unknown-source',
         title: 'What does Unknown damage source mean?',
         body: (
-            <Typography paragraph>
-                All darker tinted hitsplats <img src={TintedHitsplat} alt="Tinted Hitsplat" height={16} /> from in-game show
-                up as an Unknown source by default. However, if another player is in your{' '}
-                <Link href="https://github.com/runelite/runelite/wiki/Party" target="_blank" rel="noopener">
+            <Typography variant="body1">
+                All darker tinted hitsplats{' '}
+                <Box component="img" src={TintedHitsplat} alt="Tinted Hitsplat" sx={helpIconInlineSx}/>
+                {' '}from in-game show up as an Unknown source by default. However, if another player is in your{' '}
+                <Link href="https://github.com/runelite/runelite/wiki/Party" target="_blank" rel="noopener noreferrer">
                     Party
                 </Link>{' '}
                 with the Combat Logger plugin running, you will be able to see their hitsplats as the plugin shares it automatically.
@@ -94,20 +202,22 @@ const faq: FaqItem[] = [
         id: 'thralls',
         title: 'Are my thralls showing up as Unknown?',
         body: (
-            <Typography paragraph>
-                No, your thralls use brighter hitsplats <img src={BrightHitsplat} alt="Bright Hitsplat" height={16}/> that indicate it is your damage. There is currently no way to differentiate your thrall's damage from your damage.
+            <Typography variant="body1">
+                No, your thralls use brighter hitsplats{' '}
+                <Box component="img" src={BrightHitsplat} alt="Bright Hitsplat" sx={helpIconInlineSx}/>
+                {' '}that indicate it is your damage. There is currently no way to differentiate your thrall&apos;s damage from your damage.
             </Typography>
         ),
     },
     {
         id: 'still-unknown',
-        title:
-            'Everyone in my Party has the plugin yet I\'m still seeing some Unknown damage sources?',
+        title: 'Everyone in my Party has the plugin yet I\'m still seeing some Unknown damage sources?',
         body: (
-            <Typography paragraph>
-                Some fights will naturally have darker tinted hitsplats <img src={TintedHitsplat} alt="Tinted Hitsplat" height={16}/> whose source is
-                not able to be detected by the plugin.
-                For example, Zebak's breath that kills his own boulders uses darker tinted hitsplats and
+            <Typography variant="body1">
+                Some fights will naturally have darker tinted hitsplats{' '}
+                <Box component="img" src={TintedHitsplat} alt="Tinted Hitsplat" sx={helpIconInlineSx}/>
+                {' '}whose source is not able to be detected by the plugin.
+                For example, Zebak&apos;s breath that kills his own boulders uses darker tinted hitsplats and
                 will show up as an Unknown damage source.
                 This is a limitation of the OSRS client.
             </Typography>
