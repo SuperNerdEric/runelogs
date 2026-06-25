7915a4d9fa994ac9e2d9f7c8a5f4d15778ebcf52
Eric White <eric.white.austin@gmail.com>
2026-06-24 20:45:43 -0500

    Add new animated assets for Doom grub explosions and update configuration files
    - Added multiple animated assets for Doom grub explosions (IDs 3418 to 3425) to the asset dumper.
    - Updated assets.json to include new asset definitions with comments for each explosion.
    - Enhanced last-dump.json with detailed animation data for the new assets, including frame counts and lengths.
    - Introduced new PNG files for each explosion animation, ensuring proper rendering in the asset dumper.

--- src/lib/graphicObjectIdMap.ts
+++ src/lib/graphicObjectIdMap.ts
@@ -24,6 +24,7 @@ import o1595 from '../assets/graphicObjects/1595.png';
 
 const DOOM_SHOCKWAVE_FRAME_LENGTHS = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
 
+
 import o3405_0 from '../assets/graphicObjects/3405_0.png';
 import o3405_1 from '../assets/graphicObjects/3405_1.png';
 import o3405_2 from '../assets/graphicObjects/3405_2.png';
@@ -64,6 +65,150 @@ import o3408_6 from '../assets/graphicObjects/3408_6.png';
 import o3408_7 from '../assets/graphicObjects/3408_7.png';
 import o3408_8 from '../assets/graphicObjects/3408_8.png';
 import o3408_9 from '../assets/graphicObjects/3408_9.png';
+import o3418_0 from '../assets/graphicObjects/3418_0.png';
+import o3418_1 from '../assets/graphicObjects/3418_1.png';
+import o3418_2 from '../assets/graphicObjects/3418_2.png';
+import o3418_3 from '../assets/graphicObjects/3418_3.png';
+import o3418_4 from '../assets/graphicObjects/3418_4.png';
+import o3418_5 from '../assets/graphicObjects/3418_5.png';
+import o3418_6 from '../assets/graphicObjects/3418_6.png';
+import o3418_7 from '../assets/graphicObjects/3418_7.png';
+import o3418_8 from '../assets/graphicObjects/3418_8.png';
+import o3418_9 from '../assets/graphicObjects/3418_9.png';
+import o3418_10 from '../assets/graphicObjects/3418_10.png';
+import o3418_11 from '../assets/graphicObjects/3418_11.png';
+import o3418_12 from '../assets/graphicObjects/3418_12.png';
+import o3418_13 from '../assets/graphicObjects/3418_13.png';
+import o3418_14 from '../assets/graphicObjects/3418_14.png';
+import o3418_15 from '../assets/graphicObjects/3418_15.png';
+import o3418_16 from '../assets/graphicObjects/3418_16.png';
+import o3418_17 from '../assets/graphicObjects/3418_17.png';
+import o3419_0 from '../assets/graphicObjects/3419_0.png';
+import o3419_1 from '../assets/graphicObjects/3419_1.png';
+import o3419_2 from '../assets/graphicObjects/3419_2.png';
+import o3419_3 from '../assets/graphicObjects/3419_3.png';
+import o3419_4 from '../assets/graphicObjects/3419_4.png';
+import o3419_5 from '../assets/graphicObjects/3419_5.png';
+import o3419_6 from '../assets/graphicObjects/3419_6.png';
+import o3419_7 from '../assets/graphicObjects/3419_7.png';
+import o3419_8 from '../assets/graphicObjects/3419_8.png';
+import o3419_9 from '../assets/graphicObjects/3419_9.png';
+import o3419_10 from '../assets/graphicObjects/3419_10.png';
+import o3419_11 from '../assets/graphicObjects/3419_11.png';
+import o3419_12 from '../assets/graphicObjects/3419_12.png';
+import o3419_13 from '../assets/graphicObjects/3419_13.png';
+import o3419_14 from '../assets/graphicObjects/3419_14.png';
+import o3419_15 from '../assets/graphicObjects/3419_15.png';
+import o3419_16 from '../assets/graphicObjects/3419_16.png';
+import o3419_17 from '../assets/graphicObjects/3419_17.png';
+import o3420_0 from '../assets/graphicObjects/3420_0.png';
+import o3420_1 from '../assets/graphicObjects/3420_1.png';
+import o3420_2 from '../assets/graphicObjects/3420_2.png';
+import o3420_3 from '../assets/graphicObjects/3420_3.png';
+import o3420_4 from '../assets/graphicObjects/3420_4.png';
+import o3420_5 from '../assets/graphicObjects/3420_5.png';
+import o3420_6 from '../assets/graphicObjects/3420_6.png';
+import o3420_7 from '../assets/graphicObjects/3420_7.png';
+import o3420_8 from '../assets/graphicObjects/3420_8.png';
+import o3420_9 from '../assets/graphicObjects/3420_9.png';
+import o3420_10 from '../assets/graphicObjects/3420_10.png';
+import o3420_11 from '../assets/graphicObjects/3420_11.png';
+import o3420_12 from '../assets/graphicObjects/3420_12.png';
+import o3420_13 from '../assets/graphicObjects/3420_13.png';
+import o3420_14 from '../assets/graphicObjects/3420_14.png';
+import o3420_15 from '../assets/graphicObjects/3420_15.png';
+import o3420_16 from '../assets/graphicObjects/3420_16.png';
+import o3420_17 from '../assets/graphicObjects/3420_17.png';
+import o3421_0 from '../assets/graphicObjects/3421_0.png';
+import o3421_1 from '../assets/graphicObjects/3421_1.png';
+import o3421_2 from '../assets/graphicObjects/3421_2.png';
+import o3421_3 from '../assets/graphicObjects/3421_3.png';
+import o3421_4 from '../assets/graphicObjects/3421_4.png';
+import o3421_5 from '../assets/graphicObjects/3421_5.png';
+import o3421_6 from '../assets/graphicObjects/3421_6.png';
+import o3421_7 from '../assets/graphicObjects/3421_7.png';
+import o3421_8 from '../assets/graphicObjects/3421_8.png';
+import o3421_9 from '../assets/graphicObjects/3421_9.png';
+import o3421_10 from '../assets/graphicObjects/3421_10.png';
+import o3421_11 from '../assets/graphicObjects/3421_11.png';
+import o3421_12 from '../assets/graphicObjects/3421_12.png';
+import o3421_13 from '../assets/graphicObjects/3421_13.png';
+import o3421_14 from '../assets/graphicObjects/3421_14.png';
+import o3421_15 from '../assets/graphicObjects/3421_15.png';
+import o3421_16 from '../assets/graphicObjects/3421_16.png';
+import o3421_17 from '../assets/graphicObjects/3421_17.png';
+import o3422_0 from '../assets/graphicObjects/3422_0.png';
+import o3422_1 from '../assets/graphicObjects/3422_1.png';
+import o3422_2 from '../assets/graphicObjects/3422_2.png';
+import o3422_3 from '../assets/graphicObjects/3422_3.png';
+import o3422_4 from '../assets/graphicObjects/3422_4.png';
+import o3422_5 from '../assets/graphicObjects/3422_5.png';
+import o3422_6 from '../assets/graphicObjects/3422_6.png';
+import o3422_7 from '../assets/graphicObjects/3422_7.png';
+import o3422_8 from '../assets/graphicObjects/3422_8.png';
+import o3422_9 from '../assets/graphicObjects/3422_9.png';
+import o3422_10 from '../assets/graphicObjects/3422_10.png';
+import o3422_11 from '../assets/graphicObjects/3422_11.png';
+import o3422_12 from '../assets/graphicObjects/3422_12.png';
+import o3422_13 from '../assets/graphicObjects/3422_13.png';
+import o3422_14 from '../assets/graphicObjects/3422_14.png';
+import o3422_15 from '../assets/graphicObjects/3422_15.png';
+import o3422_16 from '../assets/graphicObjects/3422_16.png';
+import o3422_17 from '../assets/graphicObjects/3422_17.png';
+import o3423_0 from '../assets/graphicObjects/3423_0.png';
+import o3423_1 from '../assets/graphicObjects/3423_1.png';
+import o3423_2 from '../assets/graphicObjects/3423_2.png';
+import o3423_3 from '../assets/graphicObjects/3423_3.png';
+import o3423_4 from '../assets/graphicObjects/3423_4.png';
+import o3423_5 from '../assets/graphicObjects/3423_5.png';
+import o3423_6 from '../assets/graphicObjects/3423_6.png';
+import o3423_7 from '../assets/graphicObjects/3423_7.png';
+import o3423_8 from '../assets/graphicObjects/3423_8.png';
+import o3423_9 from '../assets/graphicObjects/3423_9.png';
+import o3423_10 from '../assets/graphicObjects/3423_10.png';
+import o3423_11 from '../assets/graphicObjects/3423_11.png';
+import o3423_12 from '../assets/graphicObjects/3423_12.png';
+import o3423_13 from '../assets/graphicObjects/3423_13.png';
+import o3423_14 from '../assets/graphicObjects/3423_14.png';
+import o3423_15 from '../assets/graphicObjects/3423_15.png';
+import o3423_16 from '../assets/graphicObjects/3423_16.png';
+import o3423_17 from '../assets/graphicObjects/3423_17.png';
+import o3424_0 from '../assets/graphicObjects/3424_0.png';
+import o3424_1 from '../assets/graphicObjects/3424_1.png';
+import o3424_2 from '../assets/graphicObjects/3424_2.png';
+import o3424_3 from '../assets/graphicObjects/3424_3.png';
+import o3424_4 from '../assets/graphicObjects/3424_4.png';
+import o3424_5 from '../assets/graphicObjects/3424_5.png';
+import o3424_6 from '../assets/graphicObjects/3424_6.png';
+import o3424_7 from '../assets/graphicObjects/3424_7.png';
+import o3424_8 from '../assets/graphicObjects/3424_8.png';
+import o3424_9 from '../assets/graphicObjects/3424_9.png';
+import o3424_10 from '../assets/graphicObjects/3424_10.png';
+import o3424_11 from '../assets/graphicObjects/3424_11.png';
+import o3424_12 from '../assets/graphicObjects/3424_12.png';
+import o3424_13 from '../assets/graphicObjects/3424_13.png';
+import o3424_14 from '../assets/graphicObjects/3424_14.png';
+import o3424_15 from '../assets/graphicObjects/3424_15.png';
+import o3424_16 from '../assets/graphicObjects/3424_16.png';
+import o3424_17 from '../assets/graphicObjects/3424_17.png';
+import o3425_0 from '../assets/graphicObjects/3425_0.png';
+import o3425_1 from '../assets/graphicObjects/3425_1.png';
+import o3425_2 from '../assets/graphicObjects/3425_2.png';
+import o3425_3 from '../assets/graphicObjects/3425_3.png';
+import o3425_4 from '../assets/graphicObjects/3425_4.png';
+import o3425_5 from '../assets/graphicObjects/3425_5.png';
+import o3425_6 from '../assets/graphicObjects/3425_6.png';
+import o3425_7 from '../assets/graphicObjects/3425_7.png';
+import o3425_8 from '../assets/graphicObjects/3425_8.png';
+import o3425_9 from '../assets/graphicObjects/3425_9.png';
+import o3425_10 from '../assets/graphicObjects/3425_10.png';
+import o3425_11 from '../assets/graphicObjects/3425_11.png';
+import o3425_12 from '../assets/graphicObjects/3425_12.png';
+import o3425_13 from '../assets/graphicObjects/3425_13.png';
+import o3425_14 from '../assets/graphicObjects/3425_14.png';
+import o3425_15 from '../assets/graphicObjects/3425_15.png';
+import o3425_16 from '../assets/graphicObjects/3425_16.png';
+import o3425_17 from '../assets/graphicObjects/3425_17.png';
 
 export interface GraphicObject {
     name: string;
@@ -162,5 +307,45 @@ export const graphicObjectIdMap: Record<number, GraphicObject> = {
         name: "VFX_AREA_SLAM_04",
         frameLengths: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
         frames: [o3408_0, o3408_1, o3408_2, o3408_3, o3408_4, o3408_5, o3408_6, o3408_7, o3408_8, o3408_9],
+    },
+    3418: {
+        name: "Doom grub explosion 1",
+        frameLengths: [2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5],
+        frames: [o3418_0, o3418_1, o3418_2, o3418_3, o3418_4, o3418_5, o3418_6, o3418_7, o3418_8, o3418_9, o3418_10, o3418_11, o3418_12, o3418_13, o3418_14, o3418_15, o3418_16, o3418_17],
+    },
+    3419: {
+        name: "Doom grub explosion 2",
+        frameLengths: [2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5],
+        frames: [o3419_0, o3419_1, o3419_2, o3419_3, o3419_4, o3419_5, o3419_6, o3419_7, o3419_8, o3419_9, o3419_10, o3419_11, o3419_12, o3419_13, o3419_14, o3419_15, o3419_16, o3419_17],
+    },
+    3420: {
+        name: "Doom grub explosion 3",
+        frameLengths: [2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5],
+        frames: [o3420_0, o3420_1, o3420_2, o3420_3, o3420_4, o3420_5, o3420_6, o3420_7, o3420_8, o3420_9, o3420_10, o3420_11, o3420_12, o3420_13, o3420_14, o3420_15, o3420_16, o3420_17],
+    },
+    3421: {
+        name: "Doom grub explosion 4",
+        frameLengths: [2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5],
+        frames: [o3421_0, o3421_1, o3421_2, o3421_3, o3421_4, o3421_5, o3421_6, o3421_7, o3421_8, o3421_9, o3421_10, o3421_11, o3421_12, o3421_13, o3421_14, o3421_15, o3421_16, o3421_17],
+    },
+    3422: {
+        name: "Doom grub explosion 5",
+        frameLengths: [2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5],
+        frames: [o3422_0, o3422_1, o3422_2, o3422_3, o3422_4, o3422_5, o3422_6, o3422_7, o3422_8, o3422_9, o3422_10, o3422_11, o3422_12, o3422_13, o3422_14, o3422_15, o3422_16, o3422_17],
+    },
+    3423: {
+        name: "Doom grub explosion 6",
+        frameLengths: [2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5],
+        frames: [o3423_0, o3423_1, o3423_2, o3423_3, o3423_4, o3423_5, o3423_6, o3423_7, o3423_8, o3423_9, o3423_10, o3423_11, o3423_12, o3423_13, o3423_14, o3423_15, o3423_16, o3423_17],
+    },
+    3424: {
+        name: "Doom grub explosion 7",
+        frameLengths: [2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5],
+        frames: [o3424_0, o3424_1, o3424_2, o3424_3, o3424_4, o3424_5, o3424_6, o3424_7, o3424_8, o3424_9, o3424_10, o3424_11, o3424_12, o3424_13, o3424_14, o3424_15, o3424_16, o3424_17],
+    },
+    3425: {
+        name: "Doom grub explosion 8",
+        frameLengths: [2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5],
+        frames: [o3425_0, o3425_1, o3425_2, o3425_3, o3425_4, o3425_5, o3425_6, o3425_7, o3425_8, o3425_9, o3425_10, o3425_11, o3425_12, o3425_13, o3425_14, o3425_15, o3425_16, o3425_17],
     },
 };
